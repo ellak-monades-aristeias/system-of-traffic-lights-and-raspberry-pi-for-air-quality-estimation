1b54d4e62e12d72edca88bad70e2ba76ad7d16f0
Katerina Katsaroy <katsaroukate@gmail.com>
2015-10-09 01:33:50 +0300

    save status into the database

--- src/raspberryServer/var/www/viewStatus.php
+++ src/raspberryServer/var/www/viewStatus.php
@@ -123,7 +123,7 @@
       $walkLrGreen4    = false;
       $walkLrRed4      = false;
       $walkUdGreen4    = false;
-      $walkUdRed1      = false;
+      $walkUdRed4      = false;
       if (isset($_POST['statusId'])) {
         $statusId = $_POST['statusId'];
 
@@ -287,7 +287,140 @@
           $walkUdRed4 = true;
         }
 
-        //TODO: insert to database and clean selection.
+        //Insert int database.
+        try {
+          $db = new PDO('mysql:dbname='.$dbname.';host='.$host.';port='.$port, $user, $pass);
+          $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          
+          $sql = "INSERT INTO `status`(`id`, "
+          ."`straightGreen1`, `straightYellow1`, `straightRed1`, `leftGreen1`, `leftYellow1`, `leftRed1`, `rightGreen1`, `rightYellow1`, `rightRed1`, `walkLrGreen1`, `walkLrRed1`, `walkUdGreen1`, `walkUdRed1`, "
+          ."`straightGreen2`, `straightYellow2`, `straightRed2`, `leftGreen2`, `leftYellow2`, `leftRed2`, `rightGreen2`, `rightYellow2`, `rightRed2`, `walkLrGreen2`, `walkLrRed2`, `walkUdGreen2`, `walkUdRed2`, "
+          ."`straightGreen3`, `straightYellow3`, `straightRed3`, `leftGreen3`, `leftYellow3`, `leftRed3`, `rightGreen3`, `rightYellow3`, `rightRed3`, `walkLrGreen3`, `walkLrRed3`, `walkUdGreen3`, `walkUdRed3`, "
+          ."`straightGreen4`, `straightYellow4`, `straightRed4`, `leftGreen4`, `leftYellow4`, `leftRed4`, `rightGreen4`, `rightYellow4`, `rightRed4`, `walkLrGreen4`, `walkLrRed4`, `walkUdGreen4`, `walkUdRed4`"
+          .") VALUES (:statusId, "
+          .":straightGreen1, :straightYellow1, :straightRed1, :leftGreen1, :leftYellow1, :leftRed1, :rightGreen1, :rightYellow1, :rightRed1, :walkLrGreen1, :walkLrRed1, :walkUdGreen1, :walkUdRed1, "
+          .":straightGreen2, :straightYellow2, :straightRed2, :leftGreen2, :leftYellow2, :leftRed2, :rightGreen2, :rightYellow2, :rightRed2, :walkLrGreen2, :walkLrRed2, :walkUdGreen2, :walkUdRed2, "
+          .":straightGreen3, :straightYellow3, :straightRed3, :leftGreen3, :leftYellow3, :leftRed3, :rightGreen3, :rightYellow3, :rightRed3, :walkLrGreen3, :walkLrRed3, :walkUdGreen3, :walkUdRed3, "
+          .":straightGreen4, :straightYellow4, :straightRed4, :leftGreen4, :leftYellow4, :leftRed4, :rightGreen4, :rightYellow4, :rightRed4, :walkLrGreen4, :walkLrRed4, :walkUdGreen4, :walkUdRed4"
+          .")";
+          $stmt = $db->prepare($sql);
+          $stmt->bindParam(':statusId',        $statusId);
+          $stmt->bindParam(':straightGreen1',  $straightGreen1);
+          $stmt->bindParam(':straightYellow1', $straightYellow1);
+          $stmt->bindParam(':straightRed1',    $straightRed1);
+          $stmt->bindParam(':leftGreen1',      $leftGreen1);
+          $stmt->bindParam(':leftYellow1',     $leftYellow1);
+          $stmt->bindParam(':leftRed1',        $leftRed1);
+          $stmt->bindParam(':rightGreen1',     $rightGreen1);
+          $stmt->bindParam(':rightYellow1',    $rightYellow1);
+          $stmt->bindParam(':rightRed1',       $rightRed1);
+          $stmt->bindParam(':walkLrGreen1',    $walkLrGreen1);
+          $stmt->bindParam(':walkLrRed1',      $walkLrRed1);
+          $stmt->bindParam(':walkUdGreen1',    $walkUdGreen1);
+          $stmt->bindParam(':walkUdRed1',      $walkUdRed1);
+          $stmt->bindParam(':straightGreen2',  $straightGreen2);
+          $stmt->bindParam(':straightYellow2', $straightYellow2);
+          $stmt->bindParam(':straightRed2',    $straightRed2);
+          $stmt->bindParam(':leftGreen2',      $leftGreen2);
+          $stmt->bindParam(':leftYellow2',     $leftYellow2);
+          $stmt->bindParam(':leftRed2',        $leftRed2);
+          $stmt->bindParam(':rightGreen2',     $rightGreen2);
+          $stmt->bindParam(':rightYellow2',    $rightYellow2);
+          $stmt->bindParam(':rightRed2',       $rightRed2);
+          $stmt->bindParam(':walkLrGreen2',    $walkLrGreen2);
+          $stmt->bindParam(':walkLrRed2',      $walkLrRed2);
+          $stmt->bindParam(':walkUdGreen2',    $walkUdGreen2);
+          $stmt->bindParam(':walkUdRed2',      $walkUdRed2);
+          $stmt->bindParam(':straightGreen3',  $straightGreen3);
+          $stmt->bindParam(':straightYellow3', $straightYellow3);
+          $stmt->bindParam(':straightRed3',    $straightRed3);
+          $stmt->bindParam(':leftGreen3',      $leftGreen3);
+          $stmt->bindParam(':leftYellow3',     $leftYellow3);
+          $stmt->bindParam(':leftRed3',        $leftRed3);
+          $stmt->bindParam(':rightGreen3',     $rightGreen3);
+          $stmt->bindParam(':rightYellow3',    $rightYellow3);
+          $stmt->bindParam(':rightRed3',       $rightRed3);
+          $stmt->bindParam(':walkLrGreen3',    $walkLrGreen3);
+          $stmt->bindParam(':walkLrRed3',      $walkLrRed3);
+          $stmt->bindParam(':walkUdGreen3',    $walkUdGreen3);
+          $stmt->bindParam(':walkUdRed3',      $walkUdRed3);
+          $stmt->bindParam(':straightGreen4',  $straightGreen4);
+          $stmt->bindParam(':straightYellow4', $straightYellow4);
+          $stmt->bindParam(':straightRed4',    $straightRed4);
+          $stmt->bindParam(':leftGreen4',      $leftGreen4);
+          $stmt->bindParam(':leftYellow4',     $leftYellow4);
+          $stmt->bindParam(':leftRed4',        $leftRed4);
+          $stmt->bindParam(':rightGreen4',     $rightGreen4);
+          $stmt->bindParam(':rightYellow4',    $rightYellow4);
+          $stmt->bindParam(':rightRed4',       $rightRed4);
+          $stmt->bindParam(':walkLrGreen4',    $walkLrGreen4);
+          $stmt->bindParam(':walkLrRed4',      $walkLrRed4);
+          $stmt->bindParam(':walkUdGreen4',    $walkUdGreen4);
+          $stmt->bindParam(':walkUdRed4',      $walkUdRed4);
+          $stmt->execute();
+          
+          //If executed sucessfully, do not show them on the input fields.
+          $statusId = NULL;
+          $straightGreen1  = false;
+          $straightYellow1 = false;
+          $straightRed1    = false;
+          $leftGreen1      = false;
+          $leftYellow1     = false;
+          $leftRed1        = false;
+          $rightGreen1     = false;
+          $rightYellow1    = false;
+          $rightRed1       = false;
+          $walkLrGreen1    = false;
+          $walkLrRed1      = false;
+          $walkUdGreen1    = false;
+          $walkUdRed1      = false;
+
+          $straightGreen2  = false;
+          $straightYellow2 = false;
+          $straightRed2    = false;
+          $leftGreen2      = false;
+          $leftYellow2     = false;
+          $leftRed2        = false;
+          $rightGreen2     = false;
+          $rightYellow2    = false;
+          $rightRed2       = false;
+          $walkLrGreen2    = false;
+          $walkLrRed2      = false;
+          $walkUdGreen2    = false;
+          $walkUdRed2      = false;
+
+          $straightGreen3  = false;
+          $straightYellow3 = false;
+          $straightRed3    = false;
+          $leftGreen3      = false;
+          $leftYellow3     = false;
+          $leftRed3        = false;
+          $rightGreen3     = false;
+          $rightYellow3    = false;
+          $rightRed3       = false;
+          $walkLrGreen3    = false;
+          $walkLrRed3      = false;
+          $walkUdGreen3    = false;
+          $walkUdRed3      = false;
+
+          $straightGreen4  = false;
+          $straightYellow4 = false;
+          $straightRed4    = false;
+          $leftGreen4      = false;
+          $leftYellow4     = false;
+          $leftRed4        = false;
+          $rightGreen4     = false;
+          $rightYellow4    = false;
+          $rightRed4       = false;
+          $walkLrGreen4    = false;
+          $walkLrRed4      = false;
+          $walkUdGreen4    = false;
+          $walkUdRed4      = false;
+        } catch (PDOException $e) {
+          echo 'Error in sql: ' . $e->getMessage();
+        }
+        //Close connection.
+        $dbh = null;
       }
     ?>
 
